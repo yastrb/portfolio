43aeebda61e04fa948d426c19566a187b9aee6aa
Olena Yastrebtsova <uaalena202@gmail.com>
2025-01-22 11:25:01 +0200

    feat: aurora background added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,11 @@
+import Hero from "./components/Hero"
 
 function App() {
 
 
   return (
     <>
-      <h1 className=" text-3xl font-bold underline">My Portfolio</h1>
+      <Hero/>
     </>
   )
 }
---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import { AuroraBackground } from "./ui/aurora-background"
+
+const Hero = () => {
+  return (
+    <div className=' pb-20 pt-36'>
+        <div>
+            <AuroraBackground/>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
