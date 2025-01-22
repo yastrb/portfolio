5b9fb6509e86bfad2c0b8d60af69b8b5e2910819
Olena Yastrebtsova <uaalena202@gmail.com>
2025-01-22 16:22:36 +0200

    feat: added theme switcher

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
 
   return (
     <main className=" relative flex flex-col items-center justify-center overflow-hidden mx-auto px-5 sm:px-10 bg-black">
+      <div className=" pt-6 pb-4 flex justify-end">
+        <button className=" bg-slate-800 pt-3 pb-4 px-4 rounded-xl">theme</button>
+      </div>
       <div className="w-full max-w-7xl">
         <Hero />
       </div>
