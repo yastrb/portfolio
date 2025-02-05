9884da318a61e1a003c8bd3c97b345c9f40c445c
Olena Yastrebtsova <uaalena202@gmail.com>
2025-02-05 17:05:38 +0200

    feat:add react icons

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,13 @@
 import Hero from "./components/Hero"
+import FloatingNav from "./components/ui/FloatingNav"
 function App() {
 
   return (
     <main className=" relative h-[500px] flex flex-col items-center justify-center overflow-hidden mx-auto px-5 sm:px-10 bg-black">
       <div className="w-full max-w-7xl">
+        <FloatingNav navItems={[
+          { name: "Home", link: "/", icon: "🏠" },
+        ]}/>
         <Hero />
       </div> 
     </main>
--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -4,9 +4,9 @@ const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
       <div>
-        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill='white'/>
-        <Spotlight className="top-10 left-full w-[50vw] h-[80vh]" fill='purple'/>
-        <Spotlight className="top-28 left-80 w-[50vw] h-[80vh]" fill='blue'/>
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill='blue'/>
+        <Spotlight className="top-10 left-full w-[50vw] h-[80vh]" fill='yellow'/>
+        <Spotlight className="top-28 left-80 w-[50vw] h-[80vh]" fill='yellow'/>
       </div>
     </div>
   )
