68ae1992f3033a3ec04b13ac936dcc01c9eee95d
Olena Yastrebtsova <uaalena202@gmail.com>
2025-02-05 13:34:17 +0200

    update:spotlight

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -4,7 +4,9 @@ const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
       <div>
-        <Spotlight/>
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill='white'/>
+        <Spotlight className="top-10 left-full w-[50vw] h-[80vh]" fill='purple'/>
+        <Spotlight className="top-28 left-80 w-[50vw] h-[80vh]" fill='blue'/>
       </div>
     </div>
   )
