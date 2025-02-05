6016fd34dbf22ba90162543c79290aaf5b523b79
Olena Yastrebtsova <uaalena202@gmail.com>
2025-02-05 13:23:18 +0200

    feat:add spotlight.jsx

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
 
       animation: {
         aurora: "aurora 60s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
 
       keyframes: {
@@ -26,6 +27,16 @@ module.exports = {
           },
           to: {
             backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
           },
         },
       },
