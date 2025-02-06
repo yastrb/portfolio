91aa014caeaf31ba899b17cc647180eef2043b92
Olena Yastrebtsova <uaalena202@gmail.com>
2025-02-06 18:18:01 +0200

    feat: add p1.svg to portfolio

--- portfolio/src/data/index.js
+++ portfolio/src/data/index.js
@@ -1,6 +1,21 @@
 export const gridItems = [
-    {title: "About", description: "Learn more about me", id:1, className: "md:col-span-2"},
-    {title: "About", description: "Learn more about me", id:2},
-    {title: "About", description: "Learn more about me", id:3},
-    {title: "About", description: "Learn more about me", id:4, className: "md:col-span-2"},
-]
+  {
+    title: "Hi! I'm Olena, a React.js Developer based in Ukraine",
+    description: "Tech enthusiast with a passion for development.",
+    id: 1,
+    className: "md:col-span-2",
+  },
+  {
+    title: "",
+    description: "Do you want to start a project together?",
+    id: 2,
+    img: "/public/p1.svg",
+  },
+  { title: "About", description: "Learn more about me", id: 3 },
+  {
+    title: "About",
+    description: "Learn more about me",
+    id: 4,
+    className: "md:col-span-2",
+  },
+];
